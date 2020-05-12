0e11bc205c1fe6d2b98b494586dc59a655022c62
klunico <n.klug@douglas.de>
2020-05-12 20:35:37 +0200

    modified and reduced files

--- timer-app/src/InputForm.js
+++ timer-app/src/InputForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import EventOutput from './EventOutput.js';
 import Timer from './Timer.js';
 
 
--- timer-app/src/Timer.js
+++ timer-app/src/Timer.js
@@ -6,49 +6,12 @@ const Timer = ({eventName, eventDate, eventTime, eventStarted}) => {
     		days: 0,
 	    	hours: 0,
 	    	minutes: 0,
-	    	seconds: 0
+	    	seconds: 0,
+	    	distance: 0
     });
 
 	const calculateTime = () => {
-	  // const now = new Date(eventDate,  eventTime);
-	  console.log('calculateTime start')
-	  console.log(eventName)
-	  console.log(eventDate)
-	  console.log(eventTime)
-	  const eventYear = eventDate.substr(0, 4);
-	  const eventMonth = eventDate.substr(5, 2);
-	  const eventDay = eventDate.substr(8, 2);
-	  const eventHour =	eventTime.substr(0, 2);
-	  const eventMinutes =	eventTime.substr(3, 2); 
-	  const eventSeconds = 0 ;
-	  const tomorrow = new Date( eventYear, eventMonth, eventDay, eventHour, eventMinutes, eventSeconds);
-	  
-	  console.log('calculateTime tomorrow')
-	  console.log(tomorrow)
-
-	  console.log(eventStarted)
-	  
-	  const now = new Date();
-	  const nowYear = now.getFullYear();
-	  const nowMonth = now.getMonth();
-	  const nowDate = now.getDate();
-	  const nowHour = now.getHours();
-	  const nowMintues = now.getMinutes();
-	  const nowSeconds = now.getSeconds();
-	  const calculateNow = new Date(nowYear, nowMonth, nowDate, nowHour, nowMintues, nowSeconds);
-	  console.log('calculateTime now')
-	  console.log(calculateNow)
-
-	  // console.log(now.getFullYear())
-	  // console.log(now.getMonth())
-	  // console.log(now.getDate())
-	  // console.log(now.getHours())
-	  // console.log(now.getMinutes())
-	  // console.log(now.getSeconds())
-	  // const tomorrow = eventDate?.getTime() ?? 0;
-	  
-	  const distance = tomorrow - now;
-
+	  const distance = Date.parse(eventDate, eventTime) - Date.parse(new Date());
 	  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -68,6 +31,9 @@ const Timer = ({eventName, eventDate, eventTime, eventStarted}) => {
 		}
     setInterval(() => {
       calculateTime();
+      if (this.state.distance<=0){
+      	clearInterval(calculateTime);
+      }
     } ,1000);
   },[eventStarted]);
 
@@ -83,13 +49,14 @@ const Timer = ({eventName, eventDate, eventTime, eventStarted}) => {
 	  	<div className='result'>
 				{eventName}
 				<hr />
-				<div className='day-box'>days {state.days}</div>
+				<div className='day-box'>
+				{state.days} {state.days === 1 ? 'day' : 'days'}</div>
 				<hr />
-				<div className='hour-box'>hours {state.hours}</div>
+				<div className='hour-box'>{state.hours} hours</div>
 				<hr />
-				<div className='minute-box'>minutes {state.minutes}</div>
+				<div className='minute-box'>{state.minutes} minutes</div>
 				<hr />
-				<div className='second-box'>seconds {state.seconds}</div>
+				<div className='second-box'>{state.seconds} seconds</div>
 			</div>
 		</div>
 	);
